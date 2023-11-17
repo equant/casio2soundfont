d8f649901adc80acba981e923495a4136c6d1a69
Nathanial Hendler <nathanhendler@gmail.com>
2023-11-17 14:30:44 -0700

    working pretty well, still things to do.

--- record.py
+++ record.py
@@ -1,15 +1,11 @@
 import os
+import itertools
 import numpy as np
 import sounddevice as sd
 from scipy.io.wavfile import write
 import time
 import yaml
 
-# Load configuration
-with open('config.yaml', 'r') as f:
-    config = yaml.safe_load(f)
-synth_name = config['synth_name']
-presets = config['presets']
 
 print(sd.query_devices())
 
@@ -20,6 +16,25 @@ MAX_RECORD_SECONDS = 10
 START_THRESHOLD    = 0.01
 STOP_THRESHOLD     = 0.005
 WAIT_TIMEOUT       = 20
+
+def get_white_keys(start, end):
+    all_notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+    start_octave = int(start[1])
+    end_octave = int(end[1])
+    notes = []
+
+    for octave in range(start_octave, end_octave + 1):
+        for note in all_notes:
+            note_name = f"{note}{octave}"
+            if note_name == start:
+                notes.append(note_name)
+            elif note_name == end:
+                notes.append(note_name)
+                return notes
+            elif notes:
+                notes.append(note_name)
+
+    return notes
 
 def record_silence(duration=5, fs=SAMPLE_RATE):
     with sd.InputStream(channels=1, samplerate=fs) as stream:
@@ -49,7 +64,6 @@ if use_max_val == 'yes':
     START_THRESHOLD = max_val * 10
     STOP_THRESHOLD  = START_THRESHOLD/2
 
-
 def record_note(fs=SAMPLE_RATE,
                 max_record_seconds=MAX_RECORD_SECONDS,
                 start_threshold=START_THRESHOLD,
@@ -61,7 +75,7 @@ def record_note(fs=SAMPLE_RATE,
         return np.max(np.abs(data)) < threshold
 
     with sd.Stream(channels=1, samplerate=fs) as stream:
-        print("Recording... play the note.")
+        #print("Recording... play the note.")
         buffer = []
         started = False
         silent_for = 0
@@ -96,35 +110,73 @@ def record_note(fs=SAMPLE_RATE,
     return np.array(buffer)
 
 
-for preset_name, settings in presets.items():
-    # Create directory for the preset
-    print()
-    print(f"{synth_name}")
-    print(f"{preset_name}")
-    preset_dir = f"{synth_name}_{preset_name}"
-    os.makedirs(preset_dir, exist_ok=True)
-    
-    for note_name, midi_code in settings['notes'].items():
-        # Prompt user to play the note
-        #input(f"Prepare to play the note {note_name}. Press Enter to start listening.")
-        print(f"Recording the note {note_name}...")
+# Load configuration
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+synth_name = config['synth_name']
+presets = config['presets']
+notes_range = config['notes']
+settings = config['settings']
 
-        # Record the audio
-        #audio = record_note(MAX_RECORD_SECONDS, FS, THRESHOLD, SILENCE_DURATION)
-        audio = record_note()
+setting_combinations = list(itertools.product(['on', 'off'], repeat=len(settings)))
+setting_dicts = []
+for combination in setting_combinations:
+    setting_dict = {settings[i]: combination[i] for i in range(len(settings))}
+    setting_dicts.append(setting_dict)
 
-        if audio is not None:
-            # Save the files
-            note_file = f"{preset_dir}/{note_name}.wav"
-            midi_file = f"{preset_dir}/{midi_code}.wav"
-            #write("recorded_note.wav", SAMPLE_RATE, audio)
-            write(note_file, SAMPLE_RATE, audio)
-            write(midi_file, SAMPLE_RATE, audio)
-            print(f"Saved {note_name} as {note_file} and MIDI {midi_code} as {midi_file}")
-        else:
-            print("Gave up waiting.")
+if len(notes_range) == 2:
+    # We grab white key notes between first and last
+    notes = get_white_keys(notes_range[0], notes_range[1])
+else:
+    notes = notes_range
 
 
+#        audio = record_note()
+#
+#        if audio is not None:
+#            # Save the files
+#            note_file = f"{preset_dir}/{note_name}.wav"
+#            midi_file = f"{preset_dir}/{midi_code}.wav"
+#            #write("recorded_note.wav", SAMPLE_RATE, audio)
+#            write(note_file, SAMPLE_RATE, audio)
+#            write(midi_file, SAMPLE_RATE, audio)
+#            print(f"Saved {note_name} as {note_file} and MIDI {midi_code} as {midi_file}")
+#        else:
+#            print("Gave up waiting.")
 
-print("All notes recorded.")
+print(f"Synth: {synth_name}")
+print()
+for preset in presets:
+    print(f"  {preset}")
+    for setting_dict in setting_dicts:
+        print(f"  {preset}")
+        print(f"  {setting_dict}")
+        input("Hit enter when you have the settings ready.")
+        good_recording = False
+        while not good_recording:
+            for note in notes:
+                print(f"    {note}")
+                # Create directories
+                dir_name = f"{synth_name}/{preset}"
+                os.makedirs(dir_name, exist_ok=True)
+
+                # Set up file name
+                filename_part = "-".join([f"{key}_{value}" for key, value in setting_dict.items()])
+                file_name = f"{preset}-{filename_part}-V127-{note}.wav"
+                file_path = os.path.join(dir_name, file_name)
+
+                # Record the audio
+                print(f"    Recording {file_name}...")
+                audio_data = record_note()
+
+                if audio_data is not None:
+                    # Save the recording
+                    write(file_path, SAMPLE_RATE, audio_data)
+                    print(f"    ...Saved {file_path}")
+                else:
+                    print("Gave up waiting.")
+            answer = input("Good?  Should we move on? (y/n)").strip().lower()
+            if answer == "y":
+                good_recording = True
+
 
