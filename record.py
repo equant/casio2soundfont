669b70ba0ad43a6095e36be74e5ad735c4c3b70b
Nathanial Hendler <nathanhendler@gmail.com>
2023-11-25 09:23:49 -0700

    working trimming

--- record.py
+++ record.py
@@ -18,13 +18,13 @@ STOP_THRESHOLD     = 0.005
 WAIT_TIMEOUT       = 20
 
 def get_white_keys(start, end):
-    all_notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+    white_keys = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
     start_octave = int(start[1])
     end_octave = int(end[1])
     notes = []
 
     for octave in range(start_octave, end_octave + 1):
-        for note in all_notes:
+        for note in white_keys:
             note_name = f"{note}{octave}"
             if note_name == start:
                 notes.append(note_name)
@@ -45,24 +45,21 @@ def record_silence(duration=5, fs=SAMPLE_RATE):
             buffer.extend(data[:, 0])
     return np.array(buffer)
 
-print("Finding noise floor...")
-silence_data = record_silence(SILENCE_DURATION, SAMPLE_RATE)
-max_val = np.max(np.abs(silence_data))
-min_val = np.min(np.abs(silence_data))
-mean_val = np.mean(np.abs(silence_data))
-print(f"Silence Test Results: Min: {min_val}, Max: {max_val}, Mean: {mean_val}")
-mean_val = np.mean(silence_data)
-std_dev = np.std(silence_data)
-sigma_3 = mean_val + 3 * std_dev
-print(f"Mean Value: {mean_val}")
-print(f"3 Sigma: {sigma_3}")
 
-# Prompt user to choose the threshold
-print(f"Current START_THRESHOLD: {START_THRESHOLD}")
-use_max_val = input(f"Do you want to use twice the Max value from silence test ({2*max_val}) as the new START_THRESHOLD? (yes/no): ").strip().lower()
-if use_max_val == 'yes':
-    START_THRESHOLD = max_val * 10
-    STOP_THRESHOLD  = START_THRESHOLD/2
+def trim_silence(audio_data, threshold):
+    # Find the first index where audio exceeds the threshold
+    start_index = next((i for i, sample in enumerate(audio_data) if abs(sample) > threshold*0.9), None)
+
+    # Find the last index where audio exceeds the threshold
+    end_index = next((i for i, sample in enumerate(reversed(audio_data)) if abs(sample) > threshold*0.333), None)
+
+    if start_index is not None and end_index is not None:
+        trimmed_audio = audio_data[start_index:-end_index]
+    else:
+        trimmed_audio = audio_data
+
+    return trimmed_audio
+
 
 def record_note(fs=SAMPLE_RATE,
                 max_record_seconds=MAX_RECORD_SECONDS,
@@ -75,7 +72,6 @@ def record_note(fs=SAMPLE_RATE,
         return np.max(np.abs(data)) < threshold
 
     with sd.Stream(channels=1, samplerate=fs) as stream:
-        #print("Recording... play the note.")
         buffer = []
         started = False
         silent_for = 0
@@ -109,20 +105,50 @@ def record_note(fs=SAMPLE_RATE,
 
     return np.array(buffer)
 
+def amplitude_to_db(amplitude):
+    return 20 * np.log10(abs(amplitude))
 
-# Load configuration
+
+##################################################
+#         Calculate Noise and Thresholds         #
+##################################################
+
+print("Finding noise floor...")
+silence_data = record_silence(SILENCE_DURATION, SAMPLE_RATE)
+max_noise_val = np.max(np.abs(silence_data))
+min_noise_val = np.min(np.abs(silence_data))
+mean_noise_val = np.mean(np.abs(silence_data))
+print(f"Silence Test Results: Min: {min_noise_val}, Max: {max_noise_val}, Mean: {mean_noise_val}")
+mean_noise_val = np.mean(silence_data)
+std_dev = np.std(silence_data)
+sigma_3 = mean_noise_val + 3 * std_dev
+print(f"Mean Value: {mean_noise_val}")
+print(f"3 Sigma: {sigma_3}")
+
+# START and STOP thresholds are used for detecting audio.  I.e., when to start and stop recording.
+# The STOP_THRESHOLD is used to calculated the TRIM_THRESHOLD, which is used to trim the audio
+# after the recording has been completed.
+
+# Prompt user to choose the threshold
+print(f"Current START_THRESHOLD: {START_THRESHOLD}")
+suggested_start_threshold = max_noise_val * 10
+use_max_val = input(f"Do you want to use 10 times the Max value from silence test ({suggested_start_threshold}) as the new START_THRESHOLD? (yes/no): ").strip().lower()
+if use_max_val == 'yes':
+    START_THRESHOLD = suggested_start_threshold
+    STOP_THRESHOLD  = START_THRESHOLD/2
+
+TRIM_THRESHOLD = STOP_THRESHOLD + max_noise_val
+
+
+##################################################
+#                 Configuration                  #
+##################################################
+
 with open('config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 synth_name = config['synth_name']
 presets = config['presets']
 notes_range = config['notes']
-settings = config['settings']
-
-setting_combinations = list(itertools.product(['on', 'off'], repeat=len(settings)))
-setting_dicts = []
-for combination in setting_combinations:
-    setting_dict = {settings[i]: combination[i] for i in range(len(settings))}
-    setting_dicts.append(setting_dict)
 
 if len(notes_range) == 2:
     # We grab white key notes between first and last
@@ -131,52 +157,38 @@ else:
     notes = notes_range
 
 
-#        audio = record_note()
-#
-#        if audio is not None:
-#            # Save the files
-#            note_file = f"{preset_dir}/{note_name}.wav"
-#            midi_file = f"{preset_dir}/{midi_code}.wav"
-#            #write("recorded_note.wav", SAMPLE_RATE, audio)
-#            write(note_file, SAMPLE_RATE, audio)
-#            write(midi_file, SAMPLE_RATE, audio)
-#            print(f"Saved {note_name} as {note_file} and MIDI {midi_code} as {midi_file}")
-#        else:
-#            print("Gave up waiting.")
+##################################################
+#                      Main                      #
+##################################################
 
 print(f"Synth: {synth_name}")
 print()
 for preset in presets:
     print(f"  {preset}")
-    for setting_dict in setting_dicts:
-        print(f"  {preset}")
-        print(f"  {setting_dict}")
-        input("Hit enter when you have the settings ready.")
-        good_recording = False
-        while not good_recording:
-            for note in notes:
-                print(f"    {note}")
-                # Create directories
-                dir_name = f"{synth_name}/{preset}"
-                os.makedirs(dir_name, exist_ok=True)
+    input("Hit enter when you have the settings ready.")
+    good_recording = False
+    while not good_recording:
+        for note in notes:
+            print(f"    {note}")
+            dir_name = f"recordings/{synth_name}/{preset}"
+            os.makedirs(dir_name, exist_ok=True)
 
-                # Set up file name
-                filename_part = "-".join([f"{key}_{value}" for key, value in setting_dict.items()])
-                file_name = f"{preset}-{filename_part}-V127-{note}.wav"
-                file_path = os.path.join(dir_name, file_name)
+            file_name = f"{preset}-{note}.wav"
+            file_path = os.path.join(dir_name, file_name)
 
-                # Record the audio
-                print(f"    Recording {file_name}...")
-                audio_data = record_note()
+            print(f"    Recording {file_name}...")
+            audio_data = record_note()
 
-                if audio_data is not None:
-                    # Save the recording
-                    write(file_path, SAMPLE_RATE, audio_data)
-                    print(f"    ...Saved {file_path}")
-                else:
-                    print("Gave up waiting.")
-            answer = input("Good?  Should we move on? (y/n)").strip().lower()
-            if answer == "y":
-                good_recording = True
-
+            if audio_data is not None:
+                peak_amplitude = np.max(np.abs(audio_data))
+                peak_db = amplitude_to_db(peak_amplitude)
+                print(f"Peak Volume Level: {peak_db} dB")
+                trimmed_audio = trim_silence(audio_data, TRIM_THRESHOLD)  # Or use another threshold based on your noise floor analysis
+                write(file_path, SAMPLE_RATE, trimmed_audio)
+                print(f"    ...Saved {file_path}")
+            else:
+                print("Gave up waiting.")
+        answer = input("Good?  Should we move on? (y/n)").strip().lower()
+        if answer == "y":
+            good_recording = True
 
